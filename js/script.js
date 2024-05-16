6ac10db258de510a58c559c6e21d74b44d9a1380
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-16 18:49:43 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 George Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George Khalil
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+
+"use strict"
+
+/*
+funsction
+ */
+
+function cookiesClicker() {
+  const cookie = document.getElementById("cookie")
+  const clickCount = document.getElementById("click-count")
+
+  // Check if there is a score in local storage, otherwise set it to 0
+  let score = parseInt(localStorage.getItem("score")) || 0
+
+  if (localStorage.cookiesClicker) {
+    localStorage.cookiesClicker = Number(localStorage.cookiesClicker) + 1
+  } else {
+    localStorage.cookiesClicker = 1
+  }
+  document.getElementById(result).innerHTML =
+    "Yoru score is:" + localStorage.cookiesClicker
+}
